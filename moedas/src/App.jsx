6d6c3357f1e0d4bd92438d818111106af5e125e0
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-15 09:58:22 -0300

    debug

--- moedas/src/App.jsx
+++ moedas/src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
-import CurrancyConverter from './components/CurrancyConverter'
+import CurrancyConverter from './components/CurrancyConverter.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
