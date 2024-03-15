c6fba7828f76981e6dfef30b2e6654b2078c9ed5
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-15 09:44:56 -0300

    React
    Download do React e do axios

---
+++ moedas/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CurrancyConverter from './components/CurrancyConverter'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <CurrancyConverter/>
+    </>
+  )
+}
+
+export default App
