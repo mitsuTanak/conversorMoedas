f038b1d3cf5d2bd1580665ac8bad8afd35bcedc5
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-03-15 10:53:35 -0300

    conversor
    código do conversor

--- moedas/src/App.jsx
+++ moedas/src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import CurrancyConverter from './components/CurrancyConverter.jsx'
 
 
 function App() {
+
+  // Estado das moesdas
+  const [rates, setRAtes] = useState(null);
+
+  // Estado para armazernar moeda de origem
+  const [FromCurrancy, setFromCurrancy] = useState("USD")
+
+  // Estado para armazenar moeda de destino
+  const [ToCurrancy, setToCurrancy] = useState("EUR")
+
+  // Estado para armazenar a ser convertido
+  const [amount, setAmount] = useState("1")
+
+  // Estado para armazenar a ser convertido
+  const [convertedAmount, setConvertedAmount] = useState(null)
 
   return (
     <>
